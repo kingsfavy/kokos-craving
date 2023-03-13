71e374daba2d69c7cf6e6fa7b1fb8dbcea7431f3
kingsfavy <122705122+kingsfavy@users.noreply.github.com>
2023-03-13 20:37:38 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
 function getValue(){
-  document.getElementById("firstelem"). innerHTML = "Making you look good and healthy is our goal thereby We bring to you the best and mouth watery small chops such as samosa, chin chin, puff, etc. our small chops are tasty and at an affordable price. also we  render home delivery. but for now places we can deliver are 'Uyo' and 'Abak' for now. our packages varries so are the prices and it is also price friendly." + "<br>" + "<br>" + "<br>" +
+  document.getElementById("firstelem"). innerHTML = "Making you look good and healthy is our goal thereby We bring to you the best and mouth watery small chops such as samosa, mosa, cocktail puff puff,stick meat, spring roll, etc. our small chops are tasty and at an affordable price. also we  render home delivery. but for now places we can deliver are 'Uyo' and 'Abak'. our packages varries so are the prices and it is also price friendly." + "<br>" + "<br>" + "<br>" +
   "FOR SMALL CHOPS" + "<br>" + "<br>" + "<br>" +
   "PACKAGES" +"________________________________________" +  "PRICES" + "<br>" + "<br>" +
-  "CHIN-CHIN" + "_________________________________________" + "#2000" + "<br>" + "<br>" +
+  "MOSA" + "_____________________________________________" + "#2000" + "<br>" + "<br>" +
   "SAMOSA" + "___________________________________________" + "#2000" + "<br>" + "<br>" +
-  "PUFF-PUFF" +"_________________________________________" +"#2000" + "<br>" + "<br>" +
-  "FISH PIE" + "___________________________________________" + "#2000" +"<br>" + "<br>" +
-   "DOUGHNUT" + "________________________________________" + "#2000" + "<br>" + "<br>" +
-    "MEAT PIE" + "___________________________________________" + "#2000" + "<br>" + "<br>" +
-     "BURGER" + "____________________________________________" + "#2000" + "<br>" +
-     "<br>" + "<br>" + "<br>" + 
-    "FOR CAKES" + "<br>" + "<br>" + "<br>" +
-       "PACKAGES" +"_______________________________________" +  "PRICES" + "<br>" + "<br>" +
-  "BIRTHDAY CAKES" + "_____________________________________________" + "#2000" + "<br>" + "<br>" +
-  "WEDDING CAKES" + "_____________________________________________" + "#2000" + "<br>" + "<br>" +
-  "VELVET CAKES" +"_____________________________________________" +"#2000" + "<br>" + "<br>" +
-  "RED VELVET CAKES" + "______________________________________________" + "#2000" +"<br>" + "<br>" +
-   "FOREST CAKES" + "_____________________________________________" + "#2000" + "<br>" + "<br>" +
-    "CHICOLATE CAKES" + "____________________________________________" + "#2000" + "<br>" + "<br>" +
-     "BLACK FOREST CAKES" + "_____________________________________________" + "#2000" + "<br>" +
+  "COCKTAIL PUFF-PUFF" +"_______________________________________________" +"#2000" + "<br>" + "<br>" +
+  "STICK MEAT" + "_______________________________________________" + "#2000" +"<br>" + "<br>" +
+   "SPRING ROLL" + "_______________________________________________" + "#2000" + "<br>" + "<br>" + "<br>" +
+
+    "FOR SMOOTHIES" + "<br>" + "<br>" + "<br>" +
+       "PACKAGES" +"________________;________________________" +  "PRICES" + "<br>" + "<br>" +
+  "VANILLA SMOOTHIE" + "_________________________________________" + "#2000" + "<br>" + "<br>" +
+  "STRAWBERRY SMOOTHIE" + "________________________________________" + "#2000" + "<br>" + "<br>" +
+  "CHOCOLATE SMOOOTHIE" + "______________________________________" + "#2000" +"<br>" + "<br>" +
+    "BANANA SMOOTHIE" + "________________________________________" + "#2000" + "<br>" + "<br>" +
+
      "<br>" + "<br>" + "<br>" +
      "feel free to place your order. 😊"
   
