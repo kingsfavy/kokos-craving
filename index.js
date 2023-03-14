919706eedb0a442fc1a3be4f843c19dba28abf9a
kingsfavy <122705122+kingsfavy@users.noreply.github.com>
2023-03-14 15:43:28 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function getValue(){
-  document.getElementById("firstelem"). innerHTML = "Making you look good and healthy is our goal thereby We bring to you the best and mouth watery small chops such as samosa, mosa, cocktail puff puff,stick meat, spring roll, etc. our small chops are tasty and at an affordable price. also we  render home delivery. but for now places we can deliver are 'Uyo' and 'Abak'. our packages varries so are the prices and it is also price friendly." + "<br>" + "<br>" + "<br>" +
-  "FOR SMALL CHOPS" + "<br>" + "<br>" + "<br>" +
+  document.getElementById("firstelem"). innerHTML = "Making you look good and healthy is our goal thereby We bring to you the best and mouth watery small chops such as samosa, plantain mosa, cocktail puff puff, stick meat,  spring roll. our small chops are tasty and at an affordable price. also we  render home delivery. but for now places we can deliver are 'Uyo' and 'Abak'. our packages varries so are the prices and it is also price friendly." + "<br>" + "<br>" + "<br>" +
+  "___________________________MENU___________________________" + "<br>" + "<br>" + "<br>" +
+  "FOR SMALL CHOPS :" + "<br>" + "<br>" + "<br>" +
   "PACKAGES" +"________________________________________" +  "PRICES" + "<br>" + "<br>" +
   "MOSA" + "_____________________________________________" + "#2000" + "<br>" + "<br>" +
   "SAMOSA" + "___________________________________________" + "#2000" + "<br>" + "<br>" +
@@ -8,14 +9,14 @@ function getValue(){
   "STICK MEAT" + "_______________________________________________" + "#2000" +"<br>" + "<br>" +
    "SPRING ROLL" + "_______________________________________________" + "#2000" + "<br>" + "<br>" + "<br>" +
 
-    "FOR SMOOTHIES" + "<br>" + "<br>" + "<br>" +
-       "PACKAGES" +"________________;________________________" +  "PRICES" + "<br>" + "<br>" +
+    "FOR SMOOTHIES :" + "<br>" + "<br>" + "<br>" +
+       "PACKAGES" +"_________________________________________" +  "PRICES" + "<br>" + "<br>" +
   "VANILLA SMOOTHIE" + "_________________________________________" + "#2000" + "<br>" + "<br>" +
   "STRAWBERRY SMOOTHIE" + "________________________________________" + "#2000" + "<br>" + "<br>" +
   "CHOCOLATE SMOOOTHIE" + "______________________________________" + "#2000" +"<br>" + "<br>" +
     "BANANA SMOOTHIE" + "________________________________________" + "#2000" + "<br>" + "<br>" +
 
-     "<br>" + "<br>" + "<br>" +
+     "<br>" + "<br>" +
      "feel free to place your orders. 😊"
-  
+  document.getElementById("firstelem").style.color = "black";
 }
